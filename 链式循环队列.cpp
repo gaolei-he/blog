2a2dce0674a59167bd4c14eb9aa79c80f4ad6f97
whoami096 <ahegaolei@163.com>
2023-03-22 11:52:37 +0800

    update

---
+++ 链式循环队列.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+typedef int Elem;
+struct Node
+{
+    Elem data;
+    Node* next;
+};
+void init(Node*& tail)
+{
+    tail->next = tail;
+}
+bool empty(Node* tail)
+{
+    return tail == tail->next;
+}
+bool pop(Node* tail, Elem& data)
+{
+    Node* p = tail->next;
+    if(!empty(tail)) {
+        data = p->data;
+        tail->next = p->next;
+        cerr << p->data << "#" << endl;
+        delete p;
+        return true;
+    }
+    return false;
+}
+void push(Node*& tail, Elem data)
+{
+    Node* p = new Node;
+    tail -> data = data;
+    p->next = tail->next;
+    tail->next = p;
+    tail = p;
+    // cerr << tail->data << "%" << endl;
+
+}
+void clear(Node*& tail)
+{
+    Elem data;
+    while(!empty(tail))
+        pop(tail, data);
+}
+int main()
+{
+    Node* tail = new Node;
+    init(tail);
+    for (int i = 0; i < 10; i++)
+    {
+        push(tail, i);
+    }
+    
+    int data;
+    clear(tail);
+    for (int i = 0; i < 10; i++)
+    {
+        push(tail, i);
+    }
+    clear(tail);
+    
+    return 0;
+}
